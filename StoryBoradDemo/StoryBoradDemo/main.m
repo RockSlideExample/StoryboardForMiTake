107c9c20f166d8cfb83ba2e7acb4f7866cb81388
Rock SU <wenshyansu@yahoo.com.tw>
2013-05-13 14:11:29 +0800

    add Example StoryBoradDemo

---
+++ StoryBoradDemo/StoryBoradDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryBoradDemo
+//
+//  Created by Rock Su on 13/5/10.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
