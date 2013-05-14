036dd70700a2d2918af4b3aaaa3e61b96f607a40
Rock SU <wenshyansu@yahoo.com.tw>
2013-05-14 14:36:48 +0800

    Add Example TabNaviUseStoryboard

---
+++ TabNaviUseStoryboard/TabNaviUseStoryboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabNaviUseStoryboard
+//
+//  Created by Rock Su on 13/5/14.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabNaviUseStoryboard/TabNaviUseStoryboard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TabNaviUseStoryboard
+//
+//  Created by Rock Su on 13/5/14.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
