b8db516ba26912495b12145552fab123892c13fe
Rock SU <wenshyansu@yahoo.com.tw>
2013-05-14 14:35:14 +0800

    add Example StoryboardRunPCExample

---
+++ StoryboardRunPCExample/StoryboardRunPCExample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  StoryboardRunPCExample
+//
+//  Created by Rock Su on 13/5/14.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)buttonPreaaed:(id)sender;
+@end
---
+++ StoryboardRunPCExample/StoryboardRunPCExample/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  StoryboardRunPCExample
+//
+//  Created by Rock Su on 13/5/14.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPreaaed:(id)sender {
+    
+//    UIStoryboard *storyboard = self.storyboard;
+//    UIViewController *otherViewController = [storyboard instantiateViewControllerWithIdentifier:@"otherViewController"];
+//    [self presentViewController:otherViewController animated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+    UIViewController *second = segue.destinationViewController;
+    [second setTitle:@"second"];
+    [[second view] setBackgroundColor:[UIColor redColor]];
+}
+
+@end
