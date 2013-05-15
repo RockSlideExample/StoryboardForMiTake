9cf38a7a81d747d2c7c88dfa2b8dad5f7e3ea39b
Rock SU <wenshyansu@yahoo.com.tw>
2013-05-15 10:55:01 +0800

    Add StoryboardTableView

---
+++ StoryboardTableView/StoryboardTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardTableView
+//
+//  Created by Rock Su on 13/5/15.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardTableView/StoryboardTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardTableView
+//
+//  Created by Rock Su on 13/5/15.
+//  Copyright (c) 2013年 Rock Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
